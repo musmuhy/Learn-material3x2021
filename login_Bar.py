fa13b86c9f80418e162ca3935ed63530fff84a57
muhy musa <135721275+musmuhy@users.noreply.github.com>
2024-03-04 22:47:39 +0100

    Update login_Bar.py

--- login_Bar.py
+++ login_Bar.py
@@ -4,7 +4,7 @@ import tkinter
 window = tkinter.Tk()
 window.title("login form")
 window.geometry('420x550')
-# window.configure(bg='#333333')
+# window.configure(bg='#333334')
 
 #geometry widget manager are pack, place, grid
 label = tkinter.label(window, text="Login")
