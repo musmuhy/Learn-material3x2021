32e87992d79ff2ded05c77e42037a08da43ce2cf
muhy musa <135721275+musmuhy@users.noreply.github.com>
2024-04-07 11:29:59 +0200

    Add files via upload

---
+++ Klausurmuhyadinmusa.java
@@ -0,0 +1,74 @@
+public class Klausurmuhyadinmusa {
+    public static void main(String[] args) {
+        String name = "Muhyadin Musa";
+        String klasse = "FAH35";
+        String module = "JCA1 Klausur";
+
+        System.out.println("Name = " + name);
+        System.out.println("Klasse = " + klasse);
+        System.out.println("Module = " + module);
+
+        // Printing the answers as they are
+        System.out.println("\nAnswers:");
+        System.out.println("1.C");
+        System.out.println("2.B");
+        System.out.println("3.A");
+        System.out.println("4.C");
+        System.out.println("5.A");
+        System.out.println("6.C");
+        System.out.println("7.A");
+        System.out.println("8.B");
+        System.out.println("9.C");
+        System.out.println("10.B");
+        System.out.println("11.C");
+        System.out.println("12.C");
+        System.out.println("13.B");
+        System.out.println("14.D");
+        System.out.println("15.D");
+        System.out.println("16.D");
+        System.out.println("17.C");
+        System.out.println("18.B");
+        System.out.println("19.D");
+        System.out.println("20.C");
+        System.out.println("21.B");
+        System.out.println("22.D");
+        System.out.println("23.A");
+        System.out.println("24.B");
+        System.out.println("25.C");
+        System.out.println("26.A");
+        System.out.println("27.A");
+        System.out.println("28.B");
+        System.out.println("29.B");
+        System.out.println("30.A");
+        System.out.println("31.D");
+        System.out.println("32.B");
+        System.out.println("33.B");
+        System.out.println("34.A");
+        System.out.println("35.B");
+        System.out.println("36.C");
+        System.out.println("37.D");
+        System.out.println("38.B");
+        System.out.println("39.D");
+        System.out.println("40.C");
+        System.out.println("41.B");
+        System.out.println("42.C");
+        System.out.println("43.B");
+        System.out.println("44.C");
+        System.out.println("45.B");
+        System.out.println("46.B");
+        System.out.println("47.C");
+        System.out.println("48.A");
+        System.out.println("49.B");
+        System.out.println("50.D");
+        System.out.println("51.C");
+        System.out.println("52.B");
+        System.out.println("53.C");
+        System.out.println("54.D");
+        System.out.println("55.BE");
+        System.out.println("56.A");
+        System.out.println("57.A");
+        System.out.println("58.CDF");
+        System.out.println("59.A");
+        System.out.println("60.D");
+    }
+}
